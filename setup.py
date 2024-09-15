e8924470c0a09013d954b09ee05b4c703476b739
Mpia M. <mpia-mimpiya.puludisu02@etud.univ-paris8.fr>
2024-09-15 08:47:50 +0200

    Ajout de MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="flask-transcrypt",
     version="0.1",
     py_modules=["flask_transcrypt"],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "Flask",
         "transcrypt"
     ],
     description="Flask extension to integrate Transcrypt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="codewithmpia",
     author_email="codewithmpia@gmail.com",
     url="https://github.com/codewithmpia/flask_transcrypt",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Licence :: OSI Approved :: MIT Licence",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Framework :: Flask"
     ],
